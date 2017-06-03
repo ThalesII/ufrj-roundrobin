8e8751425ada179ee5064d5af8cc79af8d7e08ee
ThalesII <thls.mglhs@gmail.com>
2017-06-03 20:20:53 -0300

    reimplemented events

--- events.c
+++ events.c
@@ -1,67 +1,200 @@
+#define MAX_EVENTS 1024
+#define MAX_PROCS 1024
+
+#include <stdlib.h>
+#include <limits.h>
+#include <assert.h>
 #include "events.h"
 
-#define MAX_EVENTS 1024
+typedef struct {
+	int time;
+	event_t *events;
+	size_t count;
+} proc_t;
 
-static event_t eventv[MAX_EVENTS];
-static int eventc;
+int curr_time = 0;
+int running = -1;
+int interrupt = -1;
+proc_t procs[MAX_PROCS];
+size_t proc_count = 0;
+event_t events[MAX_EVENTS];
+size_t event_count = 0;
 
-int create_event(event_t event)
+int create_proc(int begin, int duration, io_t *io, size_t count)
 {
-	if (eventc == MAX_EVENTS)
-		return 1;
+	assert(proc_count < MAX_PROCS - 1);
+	assert(event_count < MAX_EVENTS - 1);
+	assert(begin >= curr_time);
+	event_t *proc_events = malloc((count+1) * sizeof(*proc_events));
+	int pid = event_count;
 
-	eventv[eventc] = event;
-	eventc++;
-	return 0;
+	events[event_count++] = (event_t) { EV_PROCESS_BEGIN, pid, begin };
+
+	for (int i = 0; i < count; ++i) {
+		proc_events[i] = (event_t) {
+			EV_IO_BEGIN,
+			pid,
+			io[i].begin,
+			io_duration[io[i].type]
+		};
+	}
+	proc_events[count] = (event_t) { EV_PROCESS_END, pid, duration };
+
+	procs[proc_count++] = (proc_t) { 0, proc_events, count+1 };
+
+	return pid;
 }
+
+void set_running(int pid)
+{
+	running = pid;
+}
+
+void set_interrupt(int time)
+{
+	interrupt = time;
+}
+
+#include <stdio.h>
 
 size_t next_events(event_t *dest, size_t count)
 {
-	int time = eventv[0].time;
-	for (int i = 1; i < eventc; ++i)
-		if (eventv[i].time < time)
-			time = eventv[i].time;
+	int next_time = INT_MAX;
+	size_t num = 0;
+	proc_t *p = NULL;
 
-	int num = 0;
-	for (int i = 0; i < eventc; ++i) {
-		if (eventv[i].time == time) {
-			dest[num++] = eventv[i];
-			eventc--;
-			eventv[i] = eventv[eventc];
-			i--;
-		}
+	if (running != -1)
+		p = &procs[running];
 
-		if (num == count)
-			break;
+	// Find `next_time'
+
+	if (interrupt != -1)
+		next_time = interrupt;
+
+	for (int i = 0; i < event_count; ++i) {
+		if (events[i].time < next_time)
+			next_time = events[i].time;
 	}
-		
 
+	for (int i = 0; p && i < p->count; ++i) {
+		int time = curr_time + p->events[i].time - p->time;
+		if (time < next_time)
+			next_time = time;
+	}
+
+	assert(next_time >= curr_time);
+
+	// Copy events
+
+	if (interrupt == next_time && num < count) {
+		dest[num++] = (event_t) { EV_INTERRUPT, running };
+	}
+
+	for (int i = 0; i < event_count; ++i) {
+		if (events[i].time == next_time && num < count) {
+			dest[num++] = events[i];
+			event_count--;
+			events[i] = events[event_count];
+		}
+	}
+
+	for (int i = 0; p && i < p->count; ++i) {
+		int time = curr_time + p->events[i].time - p->time;
+		if (time == next_time && num < count) {
+			if (p->events[i].type == EV_IO_BEGIN) {
+				assert(event_count < MAX_EVENTS - 1);
+				events[event_count++] = (event_t) {
+					EV_IO_END,
+					running,
+					next_time + io_duration[p->events[i].type]
+				};
+			}
+			dest[num++] = (event_t) {
+				p->events[i].type,
+				p->events[i].pid,
+				time
+			};
+			p->count--;
+			p->events[i] = p->events[p->count];
+		}
+	}
+
+	curr_time = next_time;
 	return num;
 }
 
-/* Unit test */
-
-#if 0
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main(void)
 {
-	#define BUFFER_COUNT 16
-	event_t buffer[BUFFER_COUNT];
+	int pids[10];
+	io_t io[10];
+	event_t evts[10];
+	int run = -1;
 
-	srand(time(NULL));
-	for (int i = 0; i < 100; ++i)
-		create_event((event_t) { EV_PROCESS_BEGIN, rand() % 20, 0 });
+	printf("Creating processes...\n");
+	for (int i = 0; i < 10; ++i) {
+		int begin = rand() % 10;
+		int duration = rand() % 10 + 1;
+		int count = rand() % 3;
 
-	for (;;) {
-		size_t num = next_events(buffer, BUFFER_COUNT);
-		if (num == 0)
-			break;
-		printf("time = %d, num = %zu\n", buffer[0].time, num);
+		for (int i=0; i < count; ++i) {
+			io[i] = (io_t) { IO_A
+			               , rand() % duration
+			               };
+		}
+
+		pids[i] = create_proc(begin, duration, io, count);
 	}
-		
+
+	printf("Running simulation\n");
+	for (;;) {
+		int count = next_events(evts, 10);
+
+		if (count == 0)
+			break;
+
+		for (int i=0; i < count; ++i) {
+			event_t *evt = &evts[i];
+			switch (evt->type) {
+			case EV_PROCESS_BEGIN:
+				printf("%d: PROCESS BEGIN (%d)\n", evt->time, evt->pid);
+				if (run == -1) {
+					run = evt->pid;
+					set_running(evt->pid);
+				}
+				break;
+			case EV_PROCESS_END:
+				printf("%d: PROCESS END (%d)\n", evt->time, evt->pid);
+				pids[evt->pid] = -1;
+				run = -1;
+				for (int i=0; i < 10; ++i)
+					if (pids[i] >= 0)
+						run = pids[i];
+				set_running(run);
+				break;
+			case EV_IO_BEGIN:
+				printf("%d: IO BEGIN (%d)\n", evt->time, evt->pid);
+				pids[evt->pid] = -evts->pid;
+				run = -1;
+				for (int i=0; i < 10; ++i)
+					if (pids[i] >= 0)
+						run = pids[i];
+				set_running(run);
+				break;
+			case EV_IO_END:
+				printf("%d: IO END (%d)\n", evt->time, evt->pid);
+				pids[evt->pid] = evts->pid;
+				break;
+			case EV_INTERRUPT:
+				printf("%d: INTERRUPT (%d)", evt->time, evt->pid);
+				break;
+			default:
+				printf("don't know");
+			}
+		}
+	}
+
+	printf("End\n");
 	return 0;
 }
-#endif
--- events.h
+++ events.h
@@ -4,19 +4,44 @@
 #include <stddef.h>
 
 typedef enum {
+	// EV_NONE,
 	EV_PROCESS_BEGIN,
 	EV_PROCESS_END,
 	EV_IO_BEGIN,
 	EV_IO_END,
+	EV_INTERRUPT,
 } event_e;
 
 typedef struct {
 	event_e type;
-	int time;
 	int pid;
+	int time;
+	int duration;
 } event_t;
 
-int create_event(event_t event);
+typedef enum {
+	IO_A,
+	IO_B,
+} io_e;
+
+typedef struct {
+	io_e type;
+	int begin;
+} io_t;
+
+int io_duration[] =  {
+	[IO_A] = 3,
+	[IO_B] = 5,
+};
+
+int create_proc(int begin,
+                int duration,
+                io_t *io,
+                size_t count);
+
+void set_running(int pid);
+
+void set_interrupt(int time);
 
 size_t next_events(event_t *dest, size_t count);
 
