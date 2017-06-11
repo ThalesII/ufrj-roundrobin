e08ab510dfc941bcf8d4db1b08905c8143a5a5e2
Thales Magalhaes <thls.mglhs@gmail.com>
2017-06-10 21:57:04 -0300

    added more assertions

--- deque.c
+++ deque.c
@@ -1,14 +1,36 @@
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "deque.h"
 
 // TBD: `shrink'
 
+#include <stdio.h>
+
+static void deq_validate(deque_t *deque, size_t size)
+{
+	if (deque->begin == NULL) {
+		return;
+	}
+
+	assert(deque->begin < deque->end);
+	assert(deque->begin <= deque->front && deque->front <= deque->end);
+	assert(deque->begin <= deque->back && deque->back <= deque->end);
+	
+	if (size != 0) {
+		assert((deque->end - deque->begin) % size == 0);
+		assert((deque->front - deque->begin) % size == 0);
+		assert((deque->end - deque->begin) % size == 0);
+	}
+}
+
 // `size' must be adequately large to fit deque
 // when `front' == `back' deque is assumed full
-static void deq_resize(deque_t *deque, size_t size)
+static void deq_resize(deque_t *deque, size_t capacity)
 {
-	char *begin = malloc(size);
+	deq_validate(deque, 0);
+
+	char *begin = malloc(capacity);
 	char *back = begin;
 	size_t count;
 
@@ -16,20 +38,24 @@ static void deq_resize(deque_t *deque, size_t size)
 		;
 	} else if (deque->front < deque->back) {
 		count = deque->back - deque->front;
+		assert(count <= capacity);
 		memcpy(back, deque->front, count);
 		back += count;
 	} else {
 		count = deque->end - deque->front;
+		assert(count <= capacity);
 		memcpy(back, deque->front, count);
 		back += count;
+
 		count = deque->back - deque->begin;
+		assert(back - begin + count <= capacity);
 		memcpy(back, deque->begin, count);
 		back += count;
 	}
 
 	free(deque->begin);
 	deque->begin = begin;
-	deque->end = begin + size;
+	deque->end = begin + capacity;
 	deque->front = begin;
 	deque->back = back;
 }
@@ -38,45 +64,59 @@ static void deq_resize(deque_t *deque, size_t size)
 // when `front' == `back' deque is assumed empty
 void deq_push_(deque_t *deque, void *src, size_t size)
 {
-	if (deque->begin == NULL)
-		deq_resize(deque, size); // k * size
+	deq_validate(deque, size);
 
+	if (deque->begin == NULL) {
+		deq_resize(deque, 16 * size);
+	}
+	
 	memcpy(deque->back, src, size);
 	deque->back += size;
 
-	if (deque->back == deque->end)
+	if (deque->back == deque->end) {
 		deque->back = deque->begin;
+	}
 
-	if (deque->front == deque->back)
+	if (deque->front == deque->back) {
 		deq_resize(deque, 2 * (deque->end - deque->begin));
+	}
 }
 
 // deque must either have available space or be default
 // when `front' == `back' deque is assumed empty
 void deq_pushleft_(deque_t *deque, void *src, size_t size)
 {
-	if (deque->begin == NULL)
-		deq_resize(deque, size);
+	deq_validate(deque, size);
 
-	if (deque->front == deque->begin)
+	if (deque->begin == NULL) {
+		deq_resize(deque, 16 * size);
+	}
+
+	if (deque->front == deque->begin) {
 		deque->front = deque->end;
+	}
 
 	deque->front -= size;
 	memcpy(deque->front, src, size);
 
-	if (deque->front == deque->back)
+	if (deque->front == deque->back) {
 		deq_resize(deque, 2 * (deque->end - deque->begin));
+	}
 }
 
 // returns zero on success, fails when deque is empty
 // when `front' == `back' deque is assumed empty
 int deq_pop_(deque_t *deque, void *dest, size_t size)
 {
-	if (deque->front == deque->back)
-		return 1;
+	deq_validate(deque, size);
 
-	if (deque->back == deque->begin)
+	if (deque->front == deque->back) {
+		return 1;
+	}
+
+	if (deque->back == deque->begin) {
 		deque->back = deque->end;
+	}
 
 	deque->back -= size;
 	memcpy(dest, deque->back, size);
@@ -88,14 +128,18 @@ int deq_pop_(deque_t *deque, void *dest, size_t size)
 // when `front' == `back' deque is assumed empty
 int deq_popleft_(deque_t *deque, void *dest, size_t size)
 {
-	if (deque->front == deque->back)
+	deq_validate(deque, size);
+
+	if (deque->front == deque->back) {
 		return 1;
+	}
 
 	memcpy(dest, deque->front, size);
 	deque->front += size;
 
-	if (deque->front == deque->end)
+	if (deque->front == deque->end) {
 		deque->front = deque->begin;
+	}
 
 	return 0;
 }
